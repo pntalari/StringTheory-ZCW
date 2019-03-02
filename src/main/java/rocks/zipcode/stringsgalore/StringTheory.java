43c73ac783776c5dd082da5dff278f9afe3a86fa
Nirmala <nirmala.talari@gmail.com>
2019-03-02 15:25:13 -0500

    1-1Hour

--- src/main/java/rocks/zipcode/stringsgalore/StringTheory.java
+++ src/main/java/rocks/zipcode/stringsgalore/StringTheory.java
@@ -1,10 +1,71 @@
 package rocks.zipcode.stringsgalore;
 
+import java.util.InputMismatchException;
+
 public class StringTheory {
+    //  String input = "";
 
-    public StringTheory() {}
-
-
+    public StringTheory() {
+//this.input =
+    }
     // Put your code for all 70 exercises starting here.
+
+    public String getCharAt(String input, Integer pos) {
+        String str = "";
+        str += input.charAt(pos);
+
+        return "The character at position " + pos + " is " + str;
+    }
+
+    public Integer getUnicode(String input, Integer pos) {
+        return Integer.valueOf(input.charAt(pos));
+    }
+
+    public Integer getUnicodeCnt(String input) {
+        return input.codePointCount(0, input.length());
+    }
+
+    public String lexicoStrCompare(String str1, String str2) {
+        String strRet = "";
+
+        if (str1.compareTo(str2) == 0) {
+
+            strRet = str1 + " is equal to " + str2;
+        } else if (str1.compareTo(str2) < 0) {
+
+            strRet = str1 + " is less than " + str2;
+        } else if (str1.compareTo(str2) > 0) {
+
+            strRet = str1 + " is greater than " + str2;
+        }
+
+        return strRet;
+    }
+
+    public String lexicoStrCompareIgnoreCase(String str1, String str2){
+
+        String str = "";
+        if (str1.equalsIgnoreCase(str2)) {
+
+            str = str1 + " is equal to " + str2;
+        } else{
+            str = str1 + " is not equal to " + str2;
+        }
+
+        return str;
+    }
+
+    public String StringConcat(String str1, String str2){
+        return str1 + " " + str2;
+    }
+
+    public Boolean StringContains(String str, String cont){
+        return str.contains(cont);
+    }
+
+    public Boolean StringCompare(String str1, String str2){
+        return str1.equals(str2);
+    }
+
 
 }
--- src/test/java/rocks/zipcode/stringsgalore/StringTheoryTest.java
+++ src/test/java/rocks/zipcode/stringsgalore/StringTheoryTest.java
@@ -1,5 +1,8 @@
 package rocks.zipcode.stringsgalore;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StringTheoryTest {
@@ -13,4 +16,90 @@ public class StringTheoryTest {
     @org.junit.After
     public void tearDown() throws Exception {
     }
+
+    @Test
+    public void getCharTest1() {
+        String OriginalStr = "Java Exercises!";
+        String expectedStr = "The character at position 0 is J";
+        String actualStr = st.getCharAt(OriginalStr, 0);
+        Assert.assertEquals(expectedStr, actualStr);
+    }
+
+    @Test
+    public void getCharTest2() {
+        String OriginalStr = "Java Exercises!";
+        String expectedStr = "The character at position 10 is i";
+        String actualStr = st.getCharAt(OriginalStr, 10);
+        Assert.assertEquals(expectedStr, actualStr);
+    }
+
+    @Test
+    public void getUnicodeTest1() {
+        String OriginalStr = ": w3resource.com";
+        Integer expectedChar = 32;
+        Integer actualChar = st.getUnicode(OriginalStr, 1);
+        Assert.assertEquals(expectedChar, actualChar);
+    }
+
+    @Test
+    public void getUnicodeTest2() {
+        String OriginalStr = ": w3resource.com";
+        Integer expectedChar = 114;
+        Integer actualChar = st.getUnicode(OriginalStr, 9);
+        Assert.assertEquals(expectedChar, actualChar);
+    }
+
+    @Test
+    public void getUnicodeCntTest() {
+        String OriginalStr = ": w3resource.com";
+        Integer expectedCnt = 16;
+        Integer actualCnt = st.getUnicodeCnt(OriginalStr);
+        Assert.assertEquals(expectedCnt, actualCnt);
+    }
+
+    @Test
+    public void lexicoStrCompareTest() {
+        String str1 = "This is Exercise 1";
+        String str2 = "This is Exercise 2";
+        String expectedStr = "This is Exercise 1" + " is less than " + "This is Exercise 2";
+        String actualStr = st.lexicoStrCompare(str1,str2);
+        Assert.assertEquals(expectedStr,actualStr);
+    }
+
+    @Test
+    public void lexicoStrCompareIgnoreCaseTest() {
+        String str1 = "This is exercise 1";
+        String str2 = "This is Exercise 1";
+        String expectedStr = "This is exercise 1" + " is equal to " + "This is Exercise 1";
+        String actualStr = st.lexicoStrCompareIgnoreCase(str1,str2);
+        Assert.assertEquals(expectedStr,actualStr);
+    }
+
+    @Test
+    public void StringConcatTest() {
+        String str1 = "PHP Exercises and";
+        String str2 = "Python Exercises";
+        String expectedStr = "PHP Exercises and Python Exercises";
+        String actualStr = st.StringConcat(str1,str2);
+        Assert.assertEquals(expectedStr,actualStr);
+    }
+
+    @Test
+    public void StringContainsTest() {
+        String str1 = "PHP Exercises and Python Exercises";
+        String contains = "and";
+        Assert.assertTrue(st.StringContains(str1,contains));
+    }
+
+    @Test
+    public void StringCompareTest() {
+        String str1 = "example.com";
+        String str2 = "example.com";
+        String str3 = "Example.com";
+        String str4 = "example.com";
+        Assert.assertTrue(st.StringCompare(str1,str2));
+        Assert.assertFalse(st.StringCompare(str3,str4));
+    }
+
+
 }
